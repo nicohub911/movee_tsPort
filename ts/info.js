c466a57b9c458892a70cb2c28605c065e2b4ba81
nicolas <nicolaser911@gmail.com>
2024-12-11 22:38:58 -0300

    i made changes in movieinfo design and functionality (ts)

--- ts/info.js
+++ ts/info.js
@@ -8,6 +8,10 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+/*
+    genre_ids : arrays con los id de las categorias [].
+    
+*/
 const container = document.getElementById("container");
 // fetch to api
 function datafetch(url) {
@@ -15,6 +19,7 @@ function datafetch(url) {
         try {
             const response = yield fetch(url);
             const data = yield response.json();
+            console.log(data);
             return data.results; // return data  
         }
         catch (error) {
@@ -23,32 +28,64 @@ function datafetch(url) {
     });
 }
 document.addEventListener("DOMContentLoaded", () => __awaiter(void 0, void 0, void 0, function* () {
-    let datainfo = datafetch(`https://api.themoviedb.org/3/search/movie?api_key=tu_clave_api&query=${""}&language=es-ES&page=1`);
+    const idLocalData = localStorage.getItem("movieid"); // movie id
+    const nameLocalData = localStorage.getItem("moviename"); // movie name
+    // search the movies by name
+    let datainfo = yield datafetch(`https://api.themoviedb.org/3/search/movie?api_key=276866c75165f669db11c444784102a8&query=${nameLocalData}&language=es-ES&page=1`);
     let movieinfo;
-    let idLocalData = localStorage.getItem("movieid");
-    let nameLocalData = localStorage.getItem("moviename");
-    if (container) {
-        container.innerHTML = `
-            <h2>${""}</h2>
-            <div class="info_movie">
-                <aside>
-                    <img src="${""}" alt="">
-                </aside>
-                <div>
-                    <ul>
-                        <li>1</li>
-                        <li>2</li>
-                        <li>3</li>
-                        <li>4</li>
-                    </ul>
-                    <ul>
-                        <li>1</li>
-                        <li>2</li>
-                        <li>3</li>
-                        <li>4</li>
-                    </ul>
-                </div>
-            </div>
-        `;
+    for (let i = 0; i < datainfo.length; i++) {
+        // search the specific movie by id (and name)
+        if (datainfo[i].original_title == nameLocalData && datainfo[i].id == idLocalData) {
+            movieinfo = datainfo[i];
+            // define the categories
+            let catGroup = [];
+            for (const category of movieinfo.genre_ids) {
+                switch (category) {
+                    case 12:
+                        catGroup.push("aventura");
+                        break;
+                    case 27:
+                        catGroup.push("terror");
+                        break;
+                    case 28:
+                        catGroup.push("accion");
+                        break;
+                    case 35:
+                        catGroup.push("comedia");
+                        break;
+                    case 53:
+                        catGroup.push("suspenso");
+                        break;
+                    case 878:
+                        catGroup.push("ciencia ficcion");
+                        break;
+                    case 10749:
+                        catGroup.push("romance");
+                        break;
+                    default:
+                        console.log("error");
+                        break;
+                }
+            }
+            // print the movie structure in the html
+            if (container) {
+                container.innerHTML = `
+                    <h2>${movieinfo.original_title}</h2>
+                    <div class="info_movie">
+                        <aside>
+                            <img src="https://image.tmdb.org/t/p/w500${movieinfo.poster_path}" alt="poster of the movie">
+                        </aside>
+                        <div>
+                            <p>${movieinfo.overview}</p>
+                            <ul>
+                                <li>Release date: ${movieinfo.release_date}</li>
+                                <li>Vote count: ${movieinfo.vote_count}</li>
+                                <li>Categories: ${catGroup}</li>    
+                            </ul>
+                        </div>
+                    </div>
+                `;
+            }
+        }
     }
 }));
